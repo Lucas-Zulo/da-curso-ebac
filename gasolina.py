1be7c1d8b05de6c9559e4a7288d30e553dcbd419
Lucas-Zulo <lucas.zulo@yahoo.com.br>
2024-06-25 22:01:09 +0000

    version darkgrid

--- gasolina.py
+++ gasolina.py
@@ -1,14 +1,17 @@
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Lê os dados do arquivo CSV para um DataFrame
 df = pd.read_csv('gasolina.csv')
 
 # Define o estilo visual do Seaborn
-sns.set_style("whitegrid")
+sns.set_style("darkgrid")
 
 # Cria o gráfico de linha com Seaborn
-plt.figure(figsize=(9, 5))  # Define o tamanho da figura
-sns.lineplot(x='dia', y='venda', data=df, marker='o')  # Cria o gráfico de linha com marcadores circulares
+plt.figure(figsize=(10, 6))  # Define o tamanho da figura
+sns.lineplot(x='dia', y='venda', data=df, marker='*')  # Cria o gráfico de linha com marcadores circulares
 plt.title('Preço da Gasolina por Dia', fontsize=14)  # Define o título do gráfico
 plt.xlabel('Dia', fontsize=12)  # Define o rótulo do eixo x
 plt.ylabel('Preço (R$)', fontsize=12)  # Define o rótulo do eixo y
@@ -21,3 +24,4 @@ plt.savefig('gasolina.png')
 
 # Mostra o gráfico na tela
 plt.show()
+    
